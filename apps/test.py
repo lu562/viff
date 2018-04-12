5f1f6bb7451e03b52d089bd893493d456fb7d976
Donghang Lu <lu562@purdue.edu>
2018-04-12 16:35:07 -0400

    123
    123

--- apps/test.py
+++ apps/test.py
@@ -74,102 +74,102 @@ class Protocol:
     def __init__(self, runtime):
         # Save the Runtime for later use
         self.runtime = runtime
-		self.k = 256
-		self.b = 2
+	self.k = 64
+	self.b = 2
 
-		self.matrix = [[0 for x in range(self.k + 1)] for y in range(self.k + 1)]
-		self.openmatrix =[[0 for x in range(self.k + 1)] for y in range(self.k + 1)]
-		self.prefix = 0
+	self.matrix = [[0 for x in range(self.k + 1)] for y in range(self.k + 1)]
+	self.openmatrix =[[0 for x in range(self.k + 1)] for y in range(self.k + 1)]
+	self.prefix = 0
 
-	        # This is the value we will use in the protocol.
-	        self.target = 3
-	        
+        # This is the value we will use in the protocol.
+        self.target = 3
+        
 
-		Zp = GF(find_prime(2**271))
+	Zp = GF(find_prime(2**271))
 
+	if runtime.id == 1:
+	    a = runtime.shamir_share([1], Zp, self.target)
+	else:
+	    a = runtime.shamir_share([1], Zp)
+	self.a = a
+	for i in range(self.k + 1):
 		if runtime.id == 1:
-		    a = runtime.shamir_share([1], Zp, self.target)
+	    		self.matrix[0][i] = self.runtime.shamir_share([1], Zp, self.b**i)
 		else:
-		    a = runtime.shamir_share([1], Zp)
-		self.a = a
-		for i in range(self.k + 1):
-			if runtime.id == 1:
-		    		self.matrix[0][i] = self.runtime.shamir_share([1], Zp, self.b**i)
-			else:
-		    		self.matrix[0][i] = self.runtime.shamir_share([1], Zp)
-			
+	    		self.matrix[0][i] = self.runtime.shamir_share([1], Zp)
+		
 
 
 
-		for i in range(self.k + 1):
-			
-			self.openmatrix[0][i] = self.runtime.open(self.matrix[0][i])
-			
+	for i in range(self.k + 1):
+		
+		self.openmatrix[0][i] = self.runtime.open(self.matrix[0][i])
+		
 
-			
+		
 
-		self.prefix = runtime.open(a - self.matrix[0][1])
+	self.prefix = runtime.open(a - self.matrix[0][1])
 
-		list = [self.prefix,a]
-		list = list + [self.openmatrix[0][i] for i in range(self.k + 1)]
-		results = gather_shares(list)
+	list = [self.prefix,a]
+	list = list + [self.openmatrix[0][i] for i in range(self.k + 1)]
+	results = gather_shares(list)
 
-		results.addCallback(self.preprocess_ready)
+	results.addCallback(self.preprocess_ready)
 
 
 
 
     def preprocess_ready(self, results):
-		print "ready!"
-		record_start()
-		print "what"
-		self.matrix[1][0] = self.a
-		self.matrix[1][1] = self.matrix[1][0] * self.matrix[0][1]
-		#print  "%d"%results[0]
-		#print  results[1]
-		for m in range(2,self.k+1):
-			for n in range(0,m):
-				if m == 2 and n == 1:
-					print "[ab] already calculated"
+	print "ready!"
+	record_start()
+	print "what"
+	self.matrix[1][0] = self.a
+	self.matrix[1][1] = self.matrix[1][0] * self.matrix[0][1]
+	#print  "%d"%results[0]
+	#print  results[1]
+	for m in range(2,self.k+1):
+		for n in range(0,m):
+			if m == 2 and n == 1:
+				print "[ab] already calculated"
+				
+			else:
+				if (m - n) != 1 :
+					#print "once here?[%d,%d]"%(m-n,n)
+					sum = 0
+					for p in range(0,m-n):
+						sum = sum + self.matrix[m-n-1-p][n+p]
+					self.matrix[m-n][n] = results[0] * sum + self.matrix[0][m]
 					
 				else:
-					if (m - n) != 1 :
-						#print "once here?[%d,%d]"%(m-n,n)
-						sum = 0
-						for p in range(0,m-n):
-							sum = sum + self.matrix[m-n-1-p][n+p]
-						self.matrix[m-n][n] = results[0] * sum + self.matrix[0][m]
-						
-					else:
-						#print "once here?lol[%d,%d]"%(m-n,n)
-						sum = 0
+					#print "once here?lol[%d,%d]"%(m-n,n)
+					sum = 0
 
-						for p in range(0,n):
-							sum = sum + self.matrix[m-n+p][n-1-p]
-						self.matrix[m-n][n] = (-1) * results[0] * sum + self.matrix[m][0]
-		print "calculation finished"
-		record_stop()
+					for p in range(0,n):
+						sum = sum + self.matrix[m-n+p][n-1-p]
+					self.matrix[m-n][n] = (-1) * results[0] * sum + self.matrix[m][0]
+	print "calculation finished"
+	record_stop()
 
-		for i in range(1 , self.k + 1):	
-			self.openmatrix[i][0] = self.runtime.open(self.matrix[i][0])
+	for i in range(1 , self.k + 1):	
+		self.openmatrix[i][0] = self.runtime.open(self.matrix[i][0])
 
-		print "reconstruction finished"
-		list = [self.openmatrix[i][0] for i in range(1,self.k + 1)]
+	print "reconstruction finished"
+	list = [self.openmatrix[i][0] for i in range(1,self.k + 1)]
 
-		results = gather_shares(list)
+	results = gather_shares(list)
 
-		results.addCallback(self.calculation_ready)
+	results.addCallback(self.calculation_ready)
 
-		self.runtime.schedule_callback(results, lambda _: self.runtime.synchronize())
+	self.runtime.schedule_callback(results, lambda _: self.runtime.synchronize())
         self.runtime.schedule_callback(results, lambda _: self.runtime.shutdown())
 
 
     def calculation_ready(self, results):
-		print "ready to print"
-		#print self.openmatrix
-		#for i in range(1,self.k+1):
-			#print self.openmatrix[i][0]
-		#return results
+	print "ready to print"
+	#print self.openmatrix
+	#for i in range(1,self.k+1):
+		#print self.openmatrix[i][0]
+	#return results
 
 
 		
